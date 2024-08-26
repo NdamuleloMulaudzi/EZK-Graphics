05c96ebfade65d9cea02e78422fc97a0d589deb8
Ndamulelo <ndamulel.mulaudzi@gmail.com>
2024-08-26 11:22:08 +0200

    add a page structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import NavBar from "./pages/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <NavBar />
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service/:id" element={<Service />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import "../styles/NavBar.css";
+
+function Navbar() {
+  const [expandNavbar, setExpandNavbar] = React.useState(false);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
+
+  return (
+    <nav className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="logo">
+        <img src={logo} />
+      </div>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar(!expandNavbar);
+          }}
+        ></button>
+      </div>
+
+      <div className="links">
+        <Link to="/">HOME</Link>
+        <Link to="/about">ABOUT</Link>
+        <Link to="/service">WHAT WE DO</Link>
+        <Link to="/contact">CONTACT US</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
