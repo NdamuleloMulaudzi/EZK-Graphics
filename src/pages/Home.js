489cf65e92dddb94d97663c834b04ee9c7d9ab09
Ndamulelo <ndamulel.mulaudzi@gmail.com>
2024-08-26 11:57:00 +0200

    add home page structure

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
 import React from "react";
+import logo from "../assets/logo.jpg";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="content">
+        <h1 className="header">EZK Graphics</h1>
+        <h3>
+          Good designs come with <i>practice</i>; great designs come from the
+          <em> soul.</em>
+        </h3>
+        <p>
+          Welcome to EZK graphics, where creativity meets precision. We
+          specializing in CAD graphics, we transform ideas into stunning visual
+          realities through our expertise in both 2D and 3D design.Whether
+          you're dreaming up a groundbreaking architectural marvel or crafting
+          intricate product details, we bring your visions to life with
+          unparalleled accuracy and artistry. At EZK Graphics, we don’t just
+          create designs—we craft experiences that captivate and inspire!
+        </p>
+        <button className="contact-btn">Let's Connect</button>
+      </div>
+      <img src={logo} />
+    </div>
+  );
 }
 
 export default Home;
